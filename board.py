a2e89e653b09a99679f7630f9019145d0556f5c1
Darren Tan <darren.tan@spacex.com>
2019-04-23 15:59:54 -0700

    all four win types

--- board.py
+++ board.py
@@ -4,9 +4,10 @@
 
 import piece
 
+
 class Board(object):
 
-    #origin (0, 0) is top left
+    # origin (0, 0) is top left
 
     def __init__(self):
         # create board
@@ -26,102 +27,86 @@ class Board(object):
                 matching_pieces.append(piece)
         return matching_pieces
 
-    def is_occupied(self, x, y):
+    def is_occupied(self, row, col):
         """ anybody home?
-
-        x (int): x value to be placed at
-        y (int): y value to be placed at
-
+        row (int): row value to be placed at
+        col (int): col value to be placed at
         returns: bool
         """
-        return self.board[x][y] > 0
+        return self.board[row][col] > 0
 
     def is_available(self, id):
         return id in self.library
 
-
-    def place(self, id, x, y):
+    def place(self, id, row, col):
         """ place a piece down onto the board
         id (int): unique id of a piece
-        x (int): x value to be placed at
-        y (int): y value to be placed at
-
+        row (int): row to be placed at
+        col (int): col to be placed at
         """
-        if self.is_occupied(x, y):
+        if self.is_occupied(row, col):
             print "INVALID MOVE"
 
         elif self.is_available(id) is False:
             print "id={} IS NOT IN LIBRARY".format(id)
 
         else:
-            self.board[x][y] = id
+            # if it's an valid move, place the piece and remove from library
+            self.board[row][col] = id
             self.library.remove(id)
 
-        # print(self.board)
+        # show board state
         self.disp_board()
-        win_cond, win_pieces = self.horiz_win()
-        if win_cond:
-            print "you win!"
-            print "Condition: {0:4b}\nPieces: {1}".format(win_cond, win_pieces)
 
+        # check for win conditions
+        self.horiz_win()
+        self.vert_win()
+        self.diag_win_tl_to_br()
+        self.diag_win_bl_to_tr()
 
     def horiz_win(self):
         for i in range(4):
-            # xnor entire row
-            win_cond = xnor(self.board[i])
-            return win_cond, self.board[i]
+            win_cond, pieces = check_win_cond(self.board[i])
+            if win_cond:
+                self.game_over(win_cond, pieces, 'horizontal')
+                break
 
-
-
-
-
-    def is_win_cond(self):
-        winning_pieces = []
+    def vert_win(self):
         for i in range(4):
-            if not(self.board[i][0] ^ self.board[i][1] ^ self.board[i][2] ^ self.board[i][3]):
-                win_dir = 'horizontal'
-                winning_pieces.append(self.board[i])
-                print winning_pieces
-                win_cond = self.which_win_cond(winning_pieces)
-                return win_cond
+            pieces = [col[i] for col in self.board]
+            win_cond, pieces = check_win_cond(pieces)
+            if win_cond:
+                self.game_over(win_cond, pieces, 'vertical')
+                break
 
-            if not(self.board[0][i] ^ self.board[1][i] ^ self.board[2][i] ^ self.board[3][i]):
-                win_dir = 'vertical'
-                return True
+    def diag_win_tl_to_br(self):
+        pieces = []
+        for i in range(4):
+            pieces.append(self.board[i][i])
 
-            if not (self.board[0][0] ^ self.board[1][1] ^ self.board[2][2] ^ self.board[3][3]):
-                win_dir = 'diag_down'
-                return True
+        win_cond, pieces = check_win_cond(pieces)
+        if win_cond:
+            self.game_over(win_cond, pieces, 'diag_tl_to_br')
 
-            if not (self.board[3][3] ^ self.board[2][2] ^ self.board[1][1] ^ self.board[0][0]):
-                win_dir = 'diag_up'
-                return True
+    def diag_win_bl_to_tr(self):
+        pieces = []
+        for i in range(4):
+            pieces.append(self.board[3-i][i])
 
-            # horiz_win = self.board[i][0] & self.board[i][1] & self.board[i][2] & self.board[i][3]
-            # vert_win = self.board[0][i] & self.board[1][i] & self.board[2][i] & self.board[3][i]
-            # diag_down_win = self.board[0][0] & self.board[1][1] & self.board[2][2] & self.board[3][3]
-            # diag_up_win = self.board[3][3] & self.board[2][2] & self.board[1][1] & self.board[0][0]
+        win_cond, pieces = check_win_cond(pieces)
+        if win_cond:
+            self.game_over(win_cond, pieces, 'diag_bl_to_tr')
 
-        return False
+    def game_over(self, win_cond, pieces, direction):
+        prop = bin2prop(win_cond)
+        print("\n==============================\n")
+        print("Game Over!")
+        print("Win Direction: {}".format(direction))
+        print("Conditions: {}".format(prop))
+        print("Pieces: {}".format(pieces))
+        print("\nBoard:")
+        self.disp_board()
 
-
-    def which_win_cond(self, win_pieces):
-        assert len(win_pieces) == 4, "DOES NOT HAVE 4 PIECES"
-
-        return win_pieces[0] & win_pieces[1] & win_pieces[2] & win_pieces[3]
-
-    def game_over(self, win_cond):
-        if win_cond & 0b1000:
-            print "GAME OVER!!\nWIN CONDITION: SHAPE"
-
-        elif win_cond & 0b0100:
-            print "GAME OVER!!\nWIN CONDITION: COLOR"
-
-        elif win_cond & 0b0010:
-            print "GAME OVER!!\nWIN CONDITION: INDENT"
-
-        elif win_cond & 0b0001:
-            print "GAME OVER!!\nWIN CONDITION: HATCH"
 
 def create_pieces():
     library = []
@@ -131,7 +116,34 @@ def create_pieces():
     return library
 
 
-def xnor(pieces):
+def check_win_cond(pieces):
+    # make sure we're getting 4 pieces to check
+    assert len(pieces) == 4, "DID NOT PASS 4 PIECES TO CHECK"
+
+    # filter out -1's
+    for piece in pieces:
+        if piece < 0:
+            return 0, pieces
+
+    # xnor each piece with its neighbor
+    xnor_01 = xnor(pieces[0], pieces[1])
+    xnor_12 = xnor(pieces[1], pieces[2])
+    xnor_23 = xnor(pieces[2], pieces[3])
+
+    # bitwise and all the xnors together to see which match
+    win_cond = (xnor_01 & xnor_12 & xnor_23)
+
+    # print debug statements
+    # print("Pieces: {}".format(pieces))
+    # print("XNOR_01: {0:4b}".format(xnor_01))
+    # print("XNOR_12: {0:4b}".format(xnor_12))
+    # print("XNOR_23: {0:4b}".format(xnor_23))
+    # print("win_cond: {0:4b}".format(win_cond))
+
+    return win_cond, pieces
+
+
+def xnor(a, b):
     # Truth table
     # A | B | Result
     # -------------
@@ -139,55 +151,66 @@ def xnor(pieces):
     # 0 | 1 | 0
     # 1 | 0 | 0
     # 1 | 1 | 1
-
-    # filters out the -1's
-    for piece in pieces:
-        if piece < 0:
-            # if there are any -1's just return False
-            return 0
-
-    # initialize first val
-    val = pieces[0]
-    for i in range(1, len(pieces)):
-        # perform xnor on each value
-        val = 0b1111 - (val ^ pieces[i])
-        print("{0:4b}".format(val))
-    return val
-
-def bit_and(*args):
-    val = args[0]
-    for i in range(1, len(args)):
-        val = val & args[i]
-    # print(bin(val))
-    return val
+    return 0b1111 - (a ^ b)
 
 
+def bin2prop(id):
+    prop = []
+    if id & 0b1000:
+        prop.append('SHAPE')
 
+    if id & 0b0100:
+        prop.append('COLOR')
 
+    if id & 0b0010:
+        prop.append('INDENT')
+
+    if id & 0b0001:
+        prop.append('HATCH')
+
+    # print("PROP: {}".format(prop))
+
+    return prop
 
 
 def main():
     board = Board()
 
     # print(board.get_matching_pieces(0b0000))
+
     # horizontal win with zeros
-    print("Place 1")
-    board.place(4, 0, 0)
-    print("Place 2")
-    board.place(1, 0, 1)
-    print("Place 3")
-    board.place(2, 0, 2)
-    print("Place 4")
-    board.place(3, 0, 3)
-    # horizontal win
-    # board.place(15, 1, 1)
+    # board.place(4, 0, 0)
+    # board.place(1, 0, 1)
+    # board.place(2, 0, 2)
+    # board.place(3, 0, 3)
 
-    # board.place(14, 1, 2)
-
-    # board.place(13, 1, 3)
-
-    # board.place(12, 1, 0)
     #####################
+
+    # horizontal win with ones
+    # board.place(15, 1, 1)
+    # board.place(14, 1, 2)
+    # board.place(13, 1, 3)
+    # board.place(12, 1, 0)
+
+    #####################
+
+    # vertical win
+    # board.place(15, 0, 3)
+    # board.place(14, 1, 3)
+    # board.place(13, 2, 3)
+    # board.place(12, 3, 3)
+
+    # diag tl to br win
+    # board.place(2, 0, 0)
+    # board.place(3, 1, 1)
+    # board.place(6, 2, 2)
+    # board.place(7, 3, 3)
+
+    # diag bl to tr win
+    board.place(1, 3, 0)
+    board.place(3, 2, 1)
+    board.place(5, 1, 2)
+    board.place(7, 0, 3)
 
 
 if __name__ == '__main__':
